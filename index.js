39bf87b4dcef52b8dfae44a0f59ad35cd5ef12cc
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-24 02:06:40 +0100

    first server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Svetlin');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('Incoming HTTP Request');
+
+    res.write(`<h1>Hello from NodeJS Web Server</h1>`);
+
+    res.end();
+    
+});
+
+server.listen(2000);
