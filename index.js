78389956c9b02f41e53c8d1da225d50d4ef76522
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-03-24 22:13:42 +0100

    final request response

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('Incoming HTTP Request');
+    console.log(`Incoming HTTP Request -> ${req.url}`);
 
     res.writeHead(200, {
         'content-type': 'text/html',
     });
-    res.write(`<h1>Hello from NodeJS Web Server!!!</h1>`);
+    if ( req.url === '/cats'){
+        res.write('<h1>Cats!</h1>')
+    }else{
+        res.write(`<h1>Hello from NodeJS Web Server!!!</h1>`);
+    }
+    
 
     res.end();
     
